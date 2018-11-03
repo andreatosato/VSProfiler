79c8054e41acea7797e8c6d91fbd6f2543ceb279
Andrea Tosato <andrea.tosato@hotmail.it>
2018-11-03 17:51:06 +0100

    EF Example

--- EF/Program.cs
+++ EF/Program.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace EF
 {
@@ -14,17 +16,24 @@ namespace EF
             using (var db = new PerformanceContext())
             {
                 db.Database.CreateIfNotExists();
-                for (int i = 0; i < 10; i++)
+                for (int i = 0; i < 100; i++)
                 {
                     db.Blogs.Add(new Blog()
                     {
                         Name = $"Prova {i}",
                         Url = $"https://atosato.it/Prova{1}"
                     });
-                    db.SaveChanges();
-                }
 
-                Console.WriteLine($"Elementi in tabella: {db.Blogs.Count()}");
+                    db.SaveChanges();
+                    Console.WriteLine($"Elementi in tabella: {db.Blogs.Count()}");
+
+                    using (var connection = new SqlConnection(db.Database.Connection.ConnectionString))
+                    {
+                        connection.Open();
+                        var blogs = connection.Query<Blog>("Select * FROM Blogs");
+                        Console.WriteLine($"Elementi in tabella DAPPER: {blogs.Count()}");
+                    }
+                }
             }
 
 
